1e0f8dc26906ea93596d2302a628c04da1ff2b87
Mamta Sharma <123922178+Mamtasharma071@users.noreply.github.com>
2024-01-11 06:18:09 +0000

    Basics of javascript

--- 01_Basics/var_and_const.js
+++ 01_Basics/var_and_const.js
@@ -2,5 +2,9 @@ const accountId = 1234
 let accountEmail = "manudb@gppgle.com"
 var accountPassword = "12345"
 accountCity = "Jammu"
+console.table([accountCity,accountEmail,accountId,accountPassword])
 
+accountEmail = "none@google.com"
+accountPassword = "22121"
+accountCity = "punjab"
 console.table([accountCity,accountEmail,accountId,accountPassword])
