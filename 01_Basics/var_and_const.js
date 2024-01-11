91a6002f28ad0d08ad8421d86c272d1e6327aaf4
Mamta Sharma <123922178+Mamtasharma071@users.noreply.github.com>
2024-01-11 06:11:34 +0000

    add to repo

---
+++ 01_Basics/var_and_const.js
@@ -0,0 +1,6 @@
+const accountId = 1234
+let accountEmail = "manudb@gppgle.com"
+var accountPassword = "12345"
+accountCity = "Jammu"
+
+console.table([accountCity,accountEmail,accountId,accountPassword])
