a30063fc53dd1b83781eaa5c459a959d4bbfddc9
Mamta Sharma <123922178+Mamtasharma071@users.noreply.github.com>
2024-01-16 16:25:56 +0530

    Add files via upload

---
+++ 01_Basics/03_datatypes.js
@@ -0,0 +1,23 @@
+"use strict ";  // this is way so that all aur code will be treated as newer version
+
+//alert(3+3) // we are using nodejs not browser
+// code readablity is very important
+
+let name ='Mamta'
+let age=21
+let isloggin=false
+
+/* DATATYPES->
+-> number => 2 to power 53
+-> bigint
+-> string => ""
+-> boolean => true/false
+-> null => standalone value
+-> undefined
+-> symbol
+
+->object*/
+
+console.log(typeof name) // typeof is used to get the datatype of variables
+console.log(typeof age)
+console.log(typeof null)
