86a7e72fb3828577515aecee841b55d24dbddc23
Mamta Sharma <123922178+Mamtasharma071@users.noreply.github.com>
2024-01-17 19:46:03 +0530

    Add files via upload

---
+++ 01_Basics/01_beginning.js
@@ -0,0 +1,3 @@
+console.log("Mamta")
+
+/* console is simply used to print the value*/
---
+++ 01_Basics/02_let_const_var.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let accountEmail = "manudb@gppgle.com"
+var accountPassword = "12345"
+accountCity = "Jammu"
+
+/*-> const is used to declare a variable which contain constant value only
+ ->let and var is used to declare the variables 
+ ->we mostly use 'let' declare the variable because of issue in block scope and function scope
+ -> Also if we use 'var' to declare the variable then it will make it global
+ -> we also declare a variable without using any keyword*/
+
+
+console.table([accountCity,accountEmail,accountId,accountPassword])
+// this function is used so that we haven't need to write console separately for all printing all values
