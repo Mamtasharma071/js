271eec6323554ff28a6d2c43f29f196fb2022816
Mamta Sharma <123922178+Mamtasharma071@users.noreply.github.com>
2024-01-17 19:45:13 +0530

    Add files via upload
    Datatype conversion and operations

---
+++ 01_Basics/04_datatype_conversion.js
@@ -0,0 +1,25 @@
+let number="33"
+console.log(typeof number)
+
+let newNumber = Number(number) // way to convert the datatype
+console.log(typeof newNumber)
+console.log(newNumber)
+
+let value = 10
+//let NewValue = Number(value)
+//console.log(typeof NewValue)
+//console.log(NewValue)
+
+// let NewValue = String(value)
+// console.log(typeof NewValue)
+// console.log(NewValue)
+
+let NewValue = Boolean(value)
+console.log(typeof NewValue)
+console.log(NewValue)
+
+// If the value is Null are undefined
+
+let A = -1
+console.log(typeof A)
+console.log(A)
---
+++ 01_Basics/05_conversion_operations.js
@@ -0,0 +1,30 @@
+let value = 11
+let newvalue = -value  // way to write negative values
+console.log(newvalue);
+
+
+// ***************NUMBER OPERATIONS***********************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// *************STRING OPERATIONS*****************
+let str1 = "Mamta "
+let str2 = "Sharma"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2"); 
+
+// agr starting value string ho toh sbhi values pr string operation hi perform hote hai
+
+
+
